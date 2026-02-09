002755da961823c68f0abd360f7db12a2b306882
Oleksandr Mytrofanov <mitrofanov.322@gmail.com>
2026-02-09 12:51:15 +0200

    lecture_3_done

--- JS_lecture_3/js/script-5.js
+++ JS_lecture_3/js/script-5.js
@@ -27,11 +27,11 @@
 //     statement
 //   } while (condition);
 
-// let password = "";
-// do {
-//   password = prompt("Введіть пароль довший 4-х символів", "");
-// } while (password.length < 5);
-// console.log("Ввели пароль: ", password);
+let password = "";
+do {
+  password = prompt("Введіть пароль довший 4-х символів", "");
+} while (password.length < 5);
+console.log("Ввели пароль: ", password);
 
 // Цикл for
 
